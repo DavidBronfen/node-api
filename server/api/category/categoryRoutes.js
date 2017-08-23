fdcbf507c8de5fa605ad223014746453f38c0b2a
David Bronfen <davidbronfen@gmail.com>
2017-08-23 20:43:17 +0300

    Routes function has been added to the category routes file.

--- server/api/category/categoryRoutes.js
+++ server/api/category/categoryRoutes.js
@@ -2,6 +2,7 @@ var router = require('express').Router();
 var logger = require('../../util/logger');
 var controller = require('./categoryController');
 var createRoutes = require('../../util/createRoutes');
+createRoutes(controller, router);
 // setup boilerplate route just to satisfy a request
 // for building
 
