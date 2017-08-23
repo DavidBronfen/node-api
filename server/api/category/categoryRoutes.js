cf913cc5aa9b155094adc5fc23cc73bd12065466
DavidBronfen <david.bronfen@gizra.com>
2017-08-23 16:35:53 +0300

    Move the routes to a separate file in the util folder and require it in all the routes.

--- server/api/category/categoryRoutes.js
+++ server/api/category/categoryRoutes.js
@@ -1,19 +1,8 @@
 var router = require('express').Router();
 var logger = require('../../util/logger');
 var controller = require('./categoryController');
-
+var createRoutes = require(../../utils/createRoutes)
 // setup boilerplate route just to satisfy a request
 // for building
-router.param('id', function(){});
-
-router.route('/')
-  .get(controller.get)
-  .post(controller.post)
-
-router.route('/:id')
-  .get(controller.getOne)
-  .put(controller.put)
-  .delete(controller.delete)
-
 
 module.exports = router;
